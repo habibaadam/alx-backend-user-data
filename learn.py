5589fc79ac225ccd5df9fb4cdd38945f6f9ee006
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-11 18:14:57 +0000

    documenting key takeways from basic logging

--- learn.py
+++ learn.py
@@ -11,6 +11,16 @@ import logging
 
 # CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
+"""
+Setting up a configuration for the logging with logging.basicConfig
+The logged info is directed to a file called test.log
+The logging Level is set to Debug. P.S The default log
+level is set to Warning, and the level must be in capital letters
+The third argument is used for setting the format of the log
+asctime is the time of the log, levelname is the log level,
+message is the message to be logged, lineno shows the line
+at which each logging was made
+"""
 logging.basicConfig(filename='test.log', level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s:%(lineno)d')
 
