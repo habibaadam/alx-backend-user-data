835f96df82ca1e9afa979c39385f489c424cc9ac
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-11 20:52:09 +0000

    pycodestyle fixes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,6 +10,7 @@ def hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """checks if a provided password matches with a
     hashed password
