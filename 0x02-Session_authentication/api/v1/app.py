b1179fef7730a75091777f6697803bbf7218354d
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-17 13:56:03 +0000

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,12 +34,16 @@ def before_request():
         return
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
         return
     if not auth.authorization_header(request):
         abort(401)
     if not auth.current_user(request):
         abort(403)
+    if auth.authorization_header(request) and  \
+            auth.session_cookie(request) is None:
+        abort(401)
     request.current_user = auth.current_user(request)
 
 
