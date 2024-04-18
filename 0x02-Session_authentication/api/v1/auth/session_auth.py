b8c8e6d958e386a296602725a6f1cf2728f67ae7
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-18 11:37:13 +0000

    8. Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -33,3 +33,13 @@ class SessionAuth(Auth):
         if user_id:
             return User.get(user_id)
         return None
+
+    def destroy_session(self, request=None):
+        """terminates a user session"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if request is None or session_id is None or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+            return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,15 @@ def login():
         cookie_name = os.getenv('SESSION_NAME')
         response.set_cookie(cookie_name, session_id)
         return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    DELETE /api/v1/auth_session/logout
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
