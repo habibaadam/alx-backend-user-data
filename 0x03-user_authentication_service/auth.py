1ce4511c6388fb08576a3479b3ac00288b2c71ac
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-23 11:21:40 +0000

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,28 @@
 """module contains hashing method"""
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """hashes a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a user and hashes their password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
