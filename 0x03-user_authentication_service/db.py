d7d8c20c3a94bba95b54ead2057db534a50ea3b4
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-23 08:45:37 +0000

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
+from typing import Dict, List
 
 
 class DB:
@@ -38,11 +39,11 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: Dict) -> User:
         """finds a user by a given attribute"""
         if not kwargs:
             raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).one()
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
         return user
