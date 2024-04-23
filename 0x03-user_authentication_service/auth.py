6b0058f07c5412230acff2301763d319199c4d30
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-23 21:16:40 +0000

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-"""module contains hashing method"""
+
+"""A module for authentication-related routines.
+"""
 
 import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """hashes a password"""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generates a UUID.
+    """
+    return str(uuid4())
 
 
 class Auth:
@@ -17,23 +29,30 @@ class Auth:
     """
 
     def __init__(self):
-        """initializes the Auth instance"""
+        """Initializes a new Auth instance.
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """registers a user and hashes their password
-        then adds them to the database """
+        """Adds a new user to the database.
+        """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
-        """authenticates a user by email and password"""
+        """Checks if a user's login details are valid.
+        """
+        user = None
         try:
             user = self._db.find_user_by(email=email)
-            hashed_password = user.hashed_password
-            return bcrypt.checkpw(password.encode(), hashed_password)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
         except NoResultFound:
             return False
+        return False
