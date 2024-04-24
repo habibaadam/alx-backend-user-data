a660e0d042151326a407c270c86d64465be2d22e
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-24 09:06:13 +0000

    14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,5 +42,19 @@ def login() -> str:
     return response
 
 
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    Deletes and redirects a user to / page based on session_id
+    as a cookie
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
